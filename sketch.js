2037839840eec0e67b7f9bf48880a56fd6bee5db
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-01-29 16:07:31 +0000

    setup a grid with all blue squares

--- sketch.js
+++ sketch.js
@@ -1,10 +1,22 @@
-//set size to 800
 const CANVAS_SIZE = 800;
+const NUMBER_OF_CELLS = 10;
+const CELL_SIZE = CANVAS_SIZE / NUMBER_OF_CELLS;
+
+function preload() {
+}
 
 function setup() {
-  // create a canvas of size CANVAS_SIZE by CANVAS_SIZE
+  //create a canvas of 800 x 800 pixels
   createCanvas(CANVAS_SIZE, CANVAS_SIZE);
-
-  // set up background to greyscale 220
+  
+  // define a background color for the canvas
   background(220);
+
+  // loop through canvas to create grid
+  for (let row = 0; row < NUMBER_OF_CELLS; row++) {
+    for (let column = 0; column < NUMBER_OF_CELLS; column++) {
+      fill(20, 100, 200);
+      rect(row * CELL_SIZE, column * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    }
+  }
 }
